e61b479b0de8b1141b8ec5ec471abb92e609abf8
DellPK <humanplay228@gmail.com>
2021-11-08 11:07:18 +0300

    Добавьте файлы проекта.

---
+++ Security/EnterEmployees.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Security
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterEmployees.xaml
+    /// </summary>
+    public partial class EnterEmployees : Window
+    {
+        public static ObservableCollection<employee> employee { get; set; }
+        int i { get; set; }
+        public EnterEmployees()
+        {
+            InitializeComponent();
+            employee = new ObservableCollection<employee>(db_connection.connection.employee.ToList());
+            this.DataContext = this;
+            
+        }
+        private void btn_write_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var save = new logins();
+                save.id_employee = i;
+                save.login_time = DateTime.Now.TimeOfDay;
+                db_connection.connection.logins.Add(save);
+                db_connection.connection.SaveChanges();
+                MessageBox.Show("all ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка {ex}", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var a = (sender as ListView).SelectedItem as employee;
+            i = a.id_employee;
+        }
+    }
+}
---
+++ Security/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Security
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string password = "123";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (tbx_password.Password == password)
+                {
+                    MessageBox.Show("All OK");
+                    EnterEmployees enterEmployees = new EnterEmployees();
+                    enterEmployees.Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show($"Неверный пароль", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка {ex}", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
